199590617f4e4970eb3a89c06997e45745fe7c3e
千葉友紀子 <ChibaYukiko@chibayukiko-no-MacBook-Air.local>
2016-06-12 16:24:35 +0900

    index-1

--- calculator.py
+++ calculator.py
@@ -58,7 +58,7 @@ def tokenize(line):
 def evaluateMultiplyAndDivide(tokens): # * / calculate
     tokens2 = []
 
-    tokens.insert(0, {'type': 'MULTI'})　# Insert a dummy '*' token
+    tokens.insert(0, {'type': 'MULTI'}) # Insert a dummy '*' token
     index = 1
     number = 1
 
